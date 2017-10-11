aced864913dba8701abe5848c812fe172161a5a2
George Kaye <georgejkaye@googlemail.com>
2017-10-11 17:24:51 +0100

    Getting the hang of things now, extracts uri

--- service_client_socket.c
+++ service_client_socket.c
@@ -18,10 +18,10 @@
 /* why can I not use const size_t here? */
 #define buffer_size 1024
 
-typedef enum method{
+typedef enum request_method_name{
 	GET = 0,
 	HEAD = 1
-} method;
+} request_method_name;
 
 int service_client_socket (const int s, const char *const tag) {
 	char buffer[buffer_size];
@@ -29,77 +29,60 @@ int service_client_socket (const int s, const char *const tag) {
 
   printf ("new connection from %s\n", tag);
 
-  /* repeatedly read a buffer load of bytes, leaving room for the
-     terminating NUL we want to add to make using printf() possible */
 	while ((bytes = read (s, buffer, buffer_size - 1)) > 0) {
-
-		/* this code is not quite complete: a write can in this context be
-			partial and return 0<x<bytes.  realistically you don't need to
-			deal with this case unless you are writing multiple megabytes */
-    
-		/*if (write (s, buffer, bytes) != bytes) {
-			perror ("write");
-			return -1;
-		} */
     
 		/* NUL-terminal the string */
 		buffer[bytes] = '\0';
-		
-		/* special case for tidy printing: if the last two characters are
-			\r\n or the last character is \n, zap them so that the newline
-			following the quotes is the only one. */
-    
-		if (bytes >= 1 && buffer[bytes - 1] == '\n') {
-			if (bytes >= 2 && buffer[bytes - 2] == '\r') {
-				strcpy (buffer + bytes - 2, "..");
-			} else {
-				strcpy (buffer + bytes - 1, ".");
-			}
-		}
 
+		/* print received request (debug) */
+		printf("\nrequest received:\n%s", buffer);
+
+		/* variables for http fields and headers */
+		char *request_method_name;
+		size_t request_method_name_len = 0;	
+		char *request_uri;
+		size_t request_uri_len = 0;
+
+		/* find the first space, the end of the request method */
 		char *space = strchr(buffer, ' ');
-		size_t method_len = 0;	
 
+		/* if there's no space fail */
 		if(space == NULL){
 			if(write(s, "fail\n", 5) != 5){
 				perror("write");
 				return -1;
 			}
 		} else {
-				method_len = space - buffer;
+				
+			/* get the length of the method name and malloc */
+			request_method_name_len = space - buffer;
+			request_method_name = malloc(request_method_name_len * sizeof(char*));
 
-			char *method = malloc(method_len * sizeof(char*));
+			strncpy(request_method_name, buffer, request_method_name_len);
 
-			for(int i = 0; i < method_len; i++){
-				method[i] = buffer[i];
-			}
+			printf("request_method_name: %s\n", request_method_name);
 
-			enum method meth;
-
-			if(strcmp(method, "GET") == 0){
-				meth = GET;
-			} else {
-				meth = HEAD;
-			}
-
-			char * return_method = malloc(sizeof(char*) * (14 + method_len));
-			strcpy(return_method, "HTTP method: ");
-			strcat(return_method, method);		
-			strcat(return_method, "\n");
-
-			printf("%s", return_method);
-
-			if(write(s, return_method, 14 + method_len) != 14 + method_len){
-				perror("write");
-				return -1;
-			}
+			char *space = strchr(buffer + request_method_name_len + 1, ' ');
 			
-		
-	#if (__SIZE_WIDTH__ == 64 || __SIZEOF_POINTER__ == 8)
-		printf ("echoed %ld bytes back to %s, \"%s\"\n", bytes, tag, buffer);
-	#else
-		printf ("echoed %d bytes back to %s, \"%s\"\n", bytes, tag, buffer);
-	#endif
+			if(space == NULL){
+				if(write(s, "fail\n", 5) != 5){
+					perror("write");
+					return -1;
+				}			
+			} else {
+				request_uri_len = space - buffer - request_method_name_len;
+				
+				request_uri = malloc(request_uri_len * sizeof(char*));
+				
+				strncpy(request_uri, buffer + request_method_name_len + 1, request_uri_len);
+					
+				char * return_request_uri = malloc(sizeof(char*) * (14 + request_uri_len));
+				strcpy(return_request_uri, "request_uri: ");
+				strcat(return_request_uri, request_uri);		
+				strcat(return_request_uri, "\n");
+
+				printf("%s", return_request_uri);
+		}
 	}
 }
   
