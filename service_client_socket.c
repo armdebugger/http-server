8085017ba149fb5f32cc29f88f4944d23bd065a3
George Kaye <georgejkaye@googlemail.com>
2017-10-15 18:24:29 +0100

    Serves basic things but has some problems with images

--- service_client_socket.c
+++ service_client_socket.c
@@ -58,15 +58,14 @@ int service_client_socket (const int s, const char *const tag) {
 		char *http_version = NULL;
 		size_t http_version_len = 0;
 
+		int bad_request = 0;
+
 		/* find the first space, the end of the request method */
 		char *space = strchr(buffer, ' ');
 
 		/* if there's no space fail */
 		if(space == NULL){
-			if(write(s, "400", 3) != 3){
-				perror("write");
-				return -1;
-			}
+			bad_request = 1;
 		} else {
 				
 			/* get the length of the method name, extract */
@@ -83,10 +82,7 @@ int service_client_socket (const int s, const char *const tag) {
 			
 			/* if there's no more info, bad request */
 			if(space == NULL){
-				if(write(s, "400", 3) != 3){
-					perror("write");
-					return -1;
-				}			
+				bad_request = 1;
 			} else {
 
 				/* get the length of the uri, extract */
@@ -94,6 +90,7 @@ int service_client_socket (const int s, const char *const tag) {
 				
 				/* check malloc didn't break */
 				if(extract(&request_uri, request_uri_len, buffer + request_method_name_len + 1) == -1){
+					free(request_method_name);	
 					perror("malloc");
 					return -1;
 				}
@@ -107,16 +104,16 @@ int service_client_socket (const int s, const char *const tag) {
 				char *space = strchr(buffer + request_uri_len + 2, '\n');
 
 				if(space == NULL){
-					if(write(s, "400", 3) != 3){
-						perror("write");
-						return -1;
-					}
+					bad_request = 1;
 				} else {
 					
 					http_version_len = space - buffer - request_uri_len - 4;
 					
 					/* check malloc didn't break */
 					if(extract(&http_version, http_version_len, buffer + request_uri_len + request_method_name_len + 2) == -1){
+						
+						free(request_method_name);
+						free(request_uri);			
 						perror("malloc");
 						return -1;
 					}
@@ -132,19 +129,134 @@ int service_client_socket (const int s, const char *const tag) {
 			}
 		}
 
-		/* print as debugging for now */
-		printf("request_method_name: %s\n", request_method_name);
-		printf("request_uri: %s\n", request_uri);
-		printf("http_version: %s\n\n", http_version);
+		/* initialise our return variables */
+		char *return_message;
+		char *response_code;
+		char *content;
+		char *file_name;
+		char content_length[12];
 
+		if(bad_request == 1){
+
+			response_code = malloc(sizeof(char*) * 16);
+			content = malloc(sizeof(char*) * 25);
+
+			if(!content || !response_code){
+				free(content);
+				free(response_code);
+				free(request_method_name);
+				free(request_uri);			
+				free(http_version);
+				perror("malloc");
+				return -1;
+			}
+
+			strcpy(response_code, "400 Bad Request");
+			strcpy(content, "<h1>400 Bad Request</h1>");
+			sprintf(content_length, "%i", 24);
+
+		} else {
+
+			/* print as debugging for now */
+			printf("request_method_name: %s\n", request_method_name);
+			printf("request_uri: %s\n", request_uri);
+			printf("http_version: %s\n\n", http_version);
+
+			/* trim the first slash off the uri so we have the file */
+			file_name = malloc(sizeof(char*) * strlen(request_uri));
+			strcpy(file_name, &request_uri[1]);
+
+			char *ext;
+			ext = strchr(file_name, '.');
+
+			if(!ext){
+				ext = malloc(sizeof(char*) * 6);
+				strcpy(ext, ".html");
+				printf("%zu\n", strlen(file_name));
+				strcat(file_name, ".html");
+				printf("%s\n", file_name);
+			}
+
+			printf("%s\n", ext);
+			printf("%s\n", file_name);
+
+			FILE *file;
+			file = fopen(file_name, "r");
+		
+			if(file){
+
+				long size;
+
+				fseek(file, 0L, SEEK_END);
+				size = ftell(file);
+				rewind(file);
+
+				content = malloc(sizeof(char*) * (size + 1));
+				response_code = malloc(sizeof(char*) * 7);
+
+				if(!response_code || !content){
+					fclose(file);					
+					free(response_code);
+					free(content);
+					free(request_method_name);
+					free(request_uri);			
+					free(http_version);
+					perror("malloc");
+					return -1;
+				}
+
+				if(fread(content, size, 1, file) != 1){
+					fclose(file);					
+					free(response_code);
+					free(content);
+					free(request_method_name);
+					free(request_uri);			
+					free(http_version);
+					perror("read");
+					return -1;
+				}
+
+				fclose(file);
+				strcpy(response_code, "200 OK");
+				sprintf(content_length, "%lu", size + 1);
+								
+
+
+			} else {
+
+				response_code = malloc(sizeof(char*) * 14);
+				content = malloc(sizeof(char*) * 23);
+
+				if(!response_code || !content){
+					free(response_code);
+					free(content);
+					free(request_method_name);
+					free(request_uri);			
+					free(http_version);
+					perror("malloc");
+					return -1;
+				}
+
+				strcpy(response_code, "404 Not Found");
+				strcpy(content, "<h1>404 Not Found</h1>");
+				sprintf(content_length, "%i", 23);
+			}
+		}
 		
 
-		char return_message[300];
-		char response_code[20];
-		char content[1000];
-		char content_length[10];
+		return_message = malloc(sizeof(char*) * (92 + strlen(http_version) + strlen(response_code) + strlen(content_length) + strlen(content)));
 
-		//strcpy(return_message, http_version);
+		if(!return_message){
+			free(request_method_name);
+			free(request_uri);			
+			free(http_version);
+			free(response_code);
+			free(content_length);
+			free(content);
+			perror("malloc");
+			return -1;
+		}
+
 		strcpy(return_message, http_version);
 		strcat(return_message, " ");
 		strcat(return_message, response_code);
@@ -163,8 +275,16 @@ int service_client_socket (const int s, const char *const tag) {
 			perror("write");
 			return -1;
 		}
+		
+		free(request_method_name);
+		//free(request_uri);			
+		//free(http_version);
+		//free(response_code);
+		//free(content_length);
+		//free(content);
+		//free(return_message);
+		//free(file_name);
 
-	
 	}
   
 	/* bytes == 0: orderly close; bytes < 0: something went wrong */
@@ -172,7 +292,7 @@ int service_client_socket (const int s, const char *const tag) {
 		perror ("read");
 		return -1;
 	}
-  
+
 	printf ("connection from %s closed\n", tag);
 	close (s);
 	return 0;
